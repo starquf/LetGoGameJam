7e1818a3306391116540cffc6f43cd1522e99d61
starquf <starquf321@gmail.com>
2022-08-02 19:17:26 +0900

    겜메니저 이벤트매니저 추가

---
+++ Assets/01.Scripts/Managers/GameManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 싱글톤 (실질적인 Awake 함수 안에 있음)
+    #region SingleTon
+    private static GameManager _instance;
+    public static GameManager Instance 
+    {
+        get 
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("게임매니저가 존재하지 않습니다!!");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+        OnAwake();
+    }
+
+    private void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = default;
+        }
+    }
+    #endregion
+
+    // 어웨이크 대신 이거 쓰셈
+    private void OnAwake()
+    {
+        
+    }
+
+    // 여기에는 다른곳에서 참조해야되는 핸들러들 넣기
+    #region GameUtils
+
+
+    #endregion
+
+    // 씬 이동 시 반드시 해야하는 거
+    public void ResetOnSceneChanged()
+    {
+        ResetEvents();
+    }
+
+    private void ResetEvents()
+    {
+        EventManager<string>.RemoveAllEvents();
+    }
+}
