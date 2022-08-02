02a09103c27cd0f2d8db9ce328b5e46798619831
anglik0370 <seonwoo6429@gmail.com>
2022-08-02 19:12:05 +0900

    사운드 매니저 관련 임포트

---
+++ Assets/01.Scripts/VO/AudioSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AudioType
+{
+    MASTER,
+    BGM,
+    SFX,
+    MaxCount
+}
+
+[CreateAssetMenu(menuName = "SO/Audio")]
+public class AudioSO : ScriptableObject
+{
+    public string audioName;
+    public AudioType audioType;
+    public AudioClip clip;
+}
