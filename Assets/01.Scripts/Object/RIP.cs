1926d1871702f4c97cc909581fc2705f4dce135d
nexpang <rlarudgur123@gmail.com>
2022-08-05 03:39:23 +0900

    다트윈 킬

--- Assets/01.Scripts/Object/RIP.cs
+++ Assets/01.Scripts/Object/RIP.cs
@@ -71,6 +71,7 @@ public class RIP : LivingEntity, IPoolableComponent
 
     public void SetDisable()
     {
+        seq.Kill();
         GameObjectPoolManager.Instance.UnusedGameObject(gameObject);
     }
 }
