b7f966b27932bff39a6a1165298c5a3776125bd7
starquf <starquf321@gmail.com>
2022-08-04 13:33:46 +0900

    틀 잡는 중

---
+++ Assets/01.Scripts/Base/PlayerStat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    public float atk;
+    public float moveSpeed;
+    //public float 
+}
