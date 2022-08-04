e54abf7a3e1446a1854c9449a7e4f8115ad90c04
starquf <starquf321@gmail.com>
2022-08-04 14:57:22 +0900

    버그 픽스

--- Assets/01.Scripts/Player/PlayerAttack.cs
+++ Assets/01.Scripts/Player/PlayerAttack.cs
@@ -51,6 +51,8 @@ public class PlayerAttack : AttackBase
                     isShootOnce = false;
 
                     currentWeapon.Shoot(playerInput.mousePos);
+
+                    yield return weaponShootWait;
                 }
             }
             else
