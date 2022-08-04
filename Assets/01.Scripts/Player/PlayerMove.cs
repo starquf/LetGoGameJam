0adbd4da19d878845a8cd61db906e019125d96f4
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2022-08-04 13:43:30 +0900

    add PlayerMove Script, PlayerInputScript

---
+++ Assets/01.Scripts/Player/PlayerMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    public Vector2 playerAxis;
+    private Rigidbody2D rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        playerAxis = playerInput.moveDir.normalized;
+        playerInput.playerState = PlayerState.Move;
+        if (playerInput.moveDir == Vector2.zero)
+        {
+            playerInput.playerState = PlayerState.Idle;
+            playerAxis = Vector2.zero;
+        }
+
+        OnMove(playerAxis, 5f);
+    }
+
+    public void OnMove(Vector2 dir, float speed)
+    {
+        rigid.velocity = dir * speed;
+    }
+}
