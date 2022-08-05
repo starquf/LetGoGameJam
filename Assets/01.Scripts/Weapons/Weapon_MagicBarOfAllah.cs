b3755771221958926111728efe12440e81fff2d7
anglik0370 <seonwoo6429@gmail.com>
2022-08-05 19:09:50 +0900

    머즐 플래시 이펙트

--- Assets/01.Scripts/Weapons/Weapon_MagicBarOfAllah.cs
+++ Assets/01.Scripts/Weapons/Weapon_MagicBarOfAllah.cs
@@ -18,5 +18,7 @@ public class Weapon_MagicBarOfAllah : Weapon
         //print($"총알 발싸 히히히히히 데미지 : {damage} ");
 
         GameManager.Instance.soundHandler.Play(shotSFXName);
+
+        PlayMuzzleEffect();
     }
 }
