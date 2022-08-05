e0b2776a38c5a3cd1e81509cfd913e88d1a59f96
SH.kim <smbaekr56@naver.com>
2022-08-06 03:40:01 +0900

    fix Parrying , scoresdl;kfhjlaskufgkahdfjklamfnvkalsgtjbhmkwgtfjklaGHBJNKL,ASG

---
+++ Assets/01.Scripts/UIs/ScorePlus.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ScorePlus : MonoBehaviour , IPoolableComponent
+{
+    private RectTransform rectTransform;
+    private Text text;
+
+    public void Despawned()
+    {
+    }
+
+    public void SetDisable()
+    {
+        GameObjectPoolManager.Instance.UnusedGameObject(this.gameObject);
+    }
+
+    public void Spawned()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        text = GetComponent<Text>();
+
+        text.color = new Color(1, 1, 1, 0);
+        rectTransform.anchoredPosition3D = new Vector3(315, 60, 0);
+    }
+
+    public void SetText(string _text)
+    {
+        text.text = "+" + _text;
+        DOTween.Kill(rectTransform);
+        DOTween.Kill(text);
+
+        rectTransform.DOAnchorPos3DY(0, .2f).OnComplete(() =>
+        {
+            rectTransform.DOAnchorPos3DX(0, .2f).SetEase(Ease.InExpo).SetUpdate(true);
+        }).SetUpdate(true);
+
+        text.DOFade(1f, .2f).OnComplete(() =>
+        {
+            text.DOFade(0, .2f).OnComplete(() =>
+            {
+                SetDisable();
+            }).SetUpdate(true);
+        }).SetUpdate(true);
+    }
+}
