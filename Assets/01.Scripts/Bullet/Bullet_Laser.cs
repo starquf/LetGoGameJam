538c602e2bb8824925d227e8aee56ca59c6a6a9b
nexpang <rlarudgur123@gmail.com>
2022-08-07 06:32:34 +0900

    tlqkf

--- Assets/01.Scripts/Bullet/Bullet_Laser.cs
+++ Assets/01.Scripts/Bullet/Bullet_Laser.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class Bullet_Laser : Bullet
 {
-    public Vector3 playerBulletSize;
-    public Vector3 enemyBulletSize;
 
     public override void Spawned()
     {
         base.Spawned();
-        transform.localScale = isEnemyBullet ? enemyBulletSize : playerBulletSize;
     }
 
     public override void Despawned()
