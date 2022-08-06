d7a8bf39659d14292b7a94f6aefd132c6e55096e
nexpang <rlarudgur123@gmail.com>
2022-08-06 12:38:35 +0900

    총알 나옴 하트 나옴, 게다가 사라짐 헉!

--- Assets/01.Scripts/Object/Ammo.cs
+++ Assets/01.Scripts/Object/Ammo.cs
@@ -6,6 +6,19 @@ public class Ammo : MonoBehaviour, IPoolableComponent
 {
     public float addAmmoPersent = 5f;
 
+    private SpriteRenderer sr = null;
+
+    private bool isTimer = false;
+
+    private float defaultDestroyTimer = 30f;
+    private float destoryTimer = 0;
+    private float fadeVal = 0;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     public void Despawned()
     {
 
@@ -13,7 +26,10 @@ public class Ammo : MonoBehaviour, IPoolableComponent
 
     public void Spawned()
     {
-
+        fadeVal = 100;
+        destoryTimer = 100;
+        isTimer = false;
+        sr.color = Color.white;
     }
 
     public void SetDisable()
@@ -21,5 +37,29 @@ public class Ammo : MonoBehaviour, IPoolableComponent
         GameObjectPoolManager.Instance.UnusedGameObject(this.gameObject);
 
         gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        if(isTimer)
+        {
+            destoryTimer -= Time.deltaTime;
+            if (destoryTimer < 0f)
+            {
+                SetDisable();
+            }
+            else if (destoryTimer < 5)
+            {
+                float speed = Mathf.Clamp((50 / destoryTimer), 10, 50);
+                fadeVal += Time.deltaTime * speed;
+                sr.color = new Color(1, 1, 1, Mathf.Cos(fadeVal));
+            }
+        }
+    }
+    public void SetDestoryTimer(float time)
+    {
+        defaultDestroyTimer = time;
+        destoryTimer = defaultDestroyTimer;
+        fadeVal = 0;
+        isTimer = true;
     }
 }
--- Assets/01.Scripts/Object/Heart.cs
+++ Assets/01.Scripts/Object/Heart.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class Heart : MonoBehaviour, IPoolableComponent
 {
-    public int healVal;
+    public int healVal = 1;
+
+    private SpriteRenderer sr = null;
+
+    private bool isTimer = false;
+
+    private float defaultDestroyTimer = 30f;
+    private float destoryTimer = 0;
+    private float fadeVal = 0;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     public void Despawned()
     {
@@ -13,7 +26,10 @@ public class Heart : MonoBehaviour, IPoolableComponent
 
     public void Spawned()
     {
-
+        fadeVal = 100;
+        destoryTimer = 100;
+        isTimer = false;
+        sr.color = Color.white;
     }
 
     public void SetDisable()
@@ -21,5 +37,29 @@ public class Heart : MonoBehaviour, IPoolableComponent
         GameObjectPoolManager.Instance.UnusedGameObject(this.gameObject);
 
         gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        if (isTimer)
+        {
+            destoryTimer -= Time.deltaTime;
+            if (destoryTimer < 0f)
+            {
+                SetDisable();
+            }
+            else if (destoryTimer < 5)
+            {
+                float speed = Mathf.Clamp((50 / destoryTimer), 10, 50);
+                fadeVal += Time.deltaTime * speed;
+                sr.color = new Color(1, 1, 1, Mathf.Cos(fadeVal));
+            }
+        }
+    }
+    public void SetDestoryTimer(float time)
+    {
+        defaultDestroyTimer = time;
+        destoryTimer = defaultDestroyTimer;
+        fadeVal = 0;
+        isTimer = true;
     }
 }
