1d8d30a7fa94b39c03ffb465549be5d9cb535d0f
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2022-08-07 05:36:09 +0900

    확률 조정

--- Assets/01.Scripts/Enemy/Base/Enemy.cs
+++ Assets/01.Scripts/Enemy/Base/Enemy.cs
@@ -41,6 +41,8 @@ public class Enemy : LivingEntity, IPoolableComponent
 
     public SpriteRenderer sr;
 
+    public bool isPrintDialog;
+
     private EnemyAI enemyAI = null;
     [HideInInspector] public Weapon weapon = null;
 
@@ -49,6 +51,7 @@ public class Enemy : LivingEntity, IPoolableComponent
     public void Despawned()
     {
         enemyAI.SetActive(false);
+        isPrintDialog = false;
     }
     public override void Init()
     {
