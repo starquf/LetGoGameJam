6ec3329c4b2b909e4bcfb7020db6d903189a9f0a
anglik0370 <seonwoo6429@gmail.com>
2022-08-07 10:02:43 +0900

    피격시 일정확률로 오브젝트 생성

--- Assets/01.Scripts/Enemy/Boss/Boss.cs
+++ Assets/01.Scripts/Enemy/Boss/Boss.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Boss : LivingEntity, IPoolableComponent
 {
     public SpriteRenderer sr;
+
+    public float weaponDropRange;
+    public float weaponDropCorrectionY;
 
     protected override void Die()
     {
@@ -16,9 +20,79 @@ public class Boss : LivingEntity, IPoolableComponent
         
     }
 
+    public override void GetDamage(float damage)
+    {
+        base.GetDamage(damage);
+
+        int rand = Random.Range(0, 100);
+
+        if(rand >=0 && rand < 60)
+        {
+            
+        }
+        else if(rand >= 60 && rand < 70)
+        {
+            SpawnWeapon(WeaponType.Ak47);
+        }
+        else if(rand >= 70 && rand < 75)
+        {
+            SpawnWeapon(WeaponType.MagicBar);
+        }
+        else if(rand >= 75 && rand < 79)
+        {
+            SpawnWeapon(WeaponType.MP7);
+        }
+        else if(rand >= 79 && rand < 84)
+        {
+            SpawnWeapon(WeaponType.AWM);
+        }
+        else if(rand >= 84 && rand < 85)
+        {
+            SpawnWeapon(WeaponType.BlueArchive);
+        }
+        else if(rand >= 85 && rand < 87)
+        {
+            SpawnWeapon(WeaponType.RazerPistol);
+        }
+        else if(rand >= 87 && rand < 93)
+        {
+            SpawnWeapon(WeaponType.M870);
+        }
+        else if(rand >= 93 && rand <= 96)
+        {
+            int degree = Random.Range(0, 361);
+
+            Ammo ammo = GameObjectPoolManager.Instance.GetGameObject("Prefabs/Object/Ammo", null).GetComponent<Ammo>();
+            ammo.transform.position = new Vector2(Mathf.Cos(degree * Mathf.Deg2Rad) * weaponDropRange, Mathf.Sin(degree * Mathf.Deg2Rad) * weaponDropRange + weaponDropCorrectionY);
+            ammo.SetDestoryTimer(30f);
+        }
+        else
+        {
+            int degree = Random.Range(0, 361);
+
+            Heart h = GameObjectPoolManager.Instance.GetGameObject("Prefabs/Object/Heart", null).GetComponent<Heart>();
+            h.transform.position = new Vector2(Mathf.Cos(degree * Mathf.Deg2Rad) * weaponDropRange, Mathf.Sin(degree * Mathf.Deg2Rad) * weaponDropRange + weaponDropCorrectionY);
+            h.SetDestoryTimer(30f);
+        }
+    }
+
     public void Despawned()
     {
-        throw new System.NotImplementedException();
+        
+    }
+
+    private void SpawnWeapon(WeaponType weapon)
+    {
+        int degree = Random.Range(0, 361);
+
+        Weapon wp = GameObjectPoolManager.Instance.GetGameObject("Prefabs/Weapons/Weapon_" + weapon.ToString(), null).GetComponent<Weapon>();
+        GameManager.Instance.allItemListAdd(wp);
+
+        wp.transform.position = new Vector2(Mathf.Cos(degree * Mathf.Deg2Rad) * weaponDropRange, Mathf.Sin(degree * Mathf.Deg2Rad) * weaponDropRange + weaponDropCorrectionY);
+        wp.SetDestoryTimer(30);
+        wp.isGround = true;
+
+        wp.sr.material.SetInt("_IsActive", 1);
     }
 
     public void Spawned()
@@ -30,5 +104,10 @@ public class Boss : LivingEntity, IPoolableComponent
     public void SetDisable()
     {
         GameObjectPoolManager.Instance.UnusedGameObject(this.gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(new Vector2(transform.position.x, transform.position.y + weaponDropCorrectionY), weaponDropRange);
     }
 }
